240e211f0a842e162386fdefce0ad080bdf37727
Mark Tucker <mtucker@valuescentre.com>
2016-12-07 09:21:43 +0000

    Fix for accessing CSV files from JAR.

--- src/main/java/campaign/reader/CSVReader.java
+++ src/main/java/campaign/reader/CSVReader.java
@@ -1,10 +1,11 @@
 package campaign.reader;
 
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,9 +22,8 @@ public class CSVReader {
         String line = "";
         List<String[]> strings = new LinkedList<>();
 
-        ClassPathResource cpr = new ClassPathResource("static/"+file);
-
-        try (BufferedReader br = new BufferedReader(new FileReader(cpr.getFile()))) {
+        Resource resource = new ClassPathResource("static/"+file);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
 
             while ((line = br.readLine()) != null) {
                 String[] tokens = line.split(DELIMITER);
